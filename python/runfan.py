52508677ca2b96b0366807fc71444201ead1b795
Tony Hansen <tony.l.hansen@gmail.com>
2020-05-28 11:36:16 -0400

    pylinted

--- python/runfan.py
+++ python/runfan.py
@@ -1,47 +1,62 @@
 #!/usr/bin/env python3
 
 """
-Manage a fan on based on threshold temperatures.
+Manage a fan based on threshold temperatures.
 """
 
 # See LICENSE file for sharing purposes
-# Copyright 2019 Tony Hansen
+# Copyright 2019-2020 Tony Hansen
 
-import gpiozero, time, psutil, sys, argparse
+import argparse
+import sys
+import time
+import psutil
+import gpiozero
+
+DEF_ON = 65.0
+DEF_OFF = 55.0
+DEF_DELAY = 5.0
+DEF_GPIO = 23
 
 def main():
     """ main program execution """
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--off-threshold', "-o", type=float, default=55.0, help='Temperature threshold in degrees C to enable fan')
-    parser.add_argument('--on-threshold', "-O", type=float, default=65.0, help='Temperature threshold in degrees C to disable fan')
-    parser.add_argument('--delay', "-d", type=float, default=5.0, help='Delay, in seconds, between temperature readings')
-    parser.add_argument('--gpio', "-g", type=int, default=23, help='GPIO BCM number')
-    parser.add_argument('--verbose', "-v", action='store_true', default=False, help='Output temp and fan status messages')
+    parser.add_argument('--off-threshold', "-o", type=float, default=DEF_OFF,
+                        help=f'Temperature threshold in degrees C to enable fan, default={DEF_OFF}')
+    parser.add_argument('--on-threshold', "-O", type=float, default=DEF_ON,
+                        help=f'Temperature threshold in degrees C to disable fan, default={DEF_ON}')
+    parser.add_argument('--delay', "-d", type=float, default=DEF_DELAY,
+                        help=f'Delay, in seconds, between temperature readings, default={DEF_DELAY}')
+    parser.add_argument('--gpio', "-g", type=int, default=DEF_GPIO, help=f'GPIO BCM number, default={DEF_GPIO}')
+    parser.add_argument('--verbose', "-v", action='store_true', default=False,
+                        help='Output temp and fan status messages')
 
     args = parser.parse_args()
 
     if args.on_threshold < args.off_threshold:
-        sys.exit("--on-threshold ({}) must be greater than --off-threshold ({})".format(args.on_threshold, args.off_threshold))
+        sys.exit(f"--on-threshold {args.on_threshold} must be greater than --off-threshold {args.off_threshold}")
 
     try:
-        on = False
+        fan_on = False
         fan = gpiozero.OutputDevice(args.gpio)
         while True:
             # get CPU temperature. Some systems spell it 'cpu-thermal' and others 'cpu_thermal'
-            pt = psutil.sensors_temperatures()
-            t = pt['cpu-thermal'][0].current if 'cpu-thermal' in pt \
-                else pt['cpu_thermal'][0].current if 'cpu_thermal' in pt \
+            ptemp = psutil.sensors_temperatures()
+            cur_temp = ptemp['cpu-thermal'][0].current if 'cpu-thermal' in ptemp \
+                else ptemp['cpu_thermal'][0].current if 'cpu_thermal' in ptemp \
                      else -9999
-            if t == -9999:
+            if cur_temp == -9999:
                 print("Cannot determine the CPU temperature", file=sys.stderr)
-            print("{}: {} {}".format(time.ctime(), t, on))
+            old_fan_on = fan_on
             # if past either boundary, turn it on or off
-            if not on and t >= args.on_threshold:
+            if not fan_on and cur_temp >= args.on_threshold:
                 fan.on()
-                on = True
-            elif on and t <= args.off_threshold:
+                fan_on = True
+            elif fan_on and cur_temp <= args.off_threshold:
                 fan.off()
-                on = False
+                fan_on = False
+            if args.verbose:
+                print(f"{time.ctime()}: {cur_temp} {old_fan_on} => {fan_on}")
             time.sleep(args.delay)
     except KeyboardInterrupt:
         pass
